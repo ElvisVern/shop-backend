485adfa2c58e1c35d9e6d359a9a6e72a4514d8fc
Elvis <maoweiwei@wezonet.com>
2021-11-05 12:36:14 +0800

    change env

--- config/cache.ts
+++ config/cache.ts
@@ -1,9 +1,10 @@
 import { RedisOptions } from 'ioredis'
+import Env from '@ioc:Adonis/Core/Env'
 
 export const redisCacheConfig: RedisOptions = {
-  host: 'localhost',
-  port: 6379,
-  db: 0,
-  password: '',
-  keyPrefix: 'shop_'
+  host: Env.get('REDIS_HOST', 'localhost'),
+  port: Env.get('REDIS_PORT', '6379'),
+  db: Env.get('REDIS_DB', 0),
+  password: Env.get('REDIS_PASSWORD', ''),
+  keyPrefix: Env.get('REDIS_PREFIX', 'shop_')
 }
